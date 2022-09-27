4557c0d07d0754da46b5754e97eda85ff66016d3
Yaw Sarfo <yawsarfo2019@gmail.com>
2022-09-27 07:01:45 -0700

    manage school

---
+++ school/static/js/psw.js
@@ -0,0 +1,10 @@
+// var myinput=document.getElementById(password1);
+// var letter=document.getElementById(letter);
+// var capital=document.getElementById(capital);
+// var number=document.getElementById(number);
+// var length=document.getElementById(length);
+
+
+// myinput.onfocus=function(){
+// document.getElementById("validation_box").style.display="block";
+// }
